1dc43f191008a7f22d284294d0befd72f5b7e4f0
BhatRajendra <rajendrabhat0408@gmai.com>
2025-11-13 16:30:12 +0530

    comment out create transaction handler

--- internal/transactions/addTransactionsHandler.go
+++ internal/transactions/addTransactionsHandler.go
@@ -2,29 +2,27 @@ package transactions
 
 import (
 	"net/http"
-
-	"LedgerIt/internal/db"
-
-	"github.com/jackc/pgx/v5/pgtype"
 )
 
 func (h *Handler) AddTransactionsHandler(w http.ResponseWriter, r *http.Request) {
-	businessUuid, ok := h.ExtractBusinessUUID(w, r)
-	if !ok {
-		return
-	}
-
-	type reqType struct {
-		Amount      float64                     `json:"amount"`
-		Direction   db.NullTransactionDirection `json:"direction"`
-		Description pgtype.Text                 `json:"description"`
-		PartyID     pgtype.UUID                 `json:"party_id"`
-		Mode        db.NullTransactionMode      `json:"mode"`
-		CategoryID  pgtype.Int4                 `json:"category_id"`
-	}
-	type resType struct{}
-	transaction, err := h.db.CreateTransaction(r.Context(), db.CreateTransactionParams{
-		BusinessID: businessUuid,
-		Amount:     pgtype.Numeric{},
-	})
+	// businessUuid, ok := h.ExtractBusinessUUID(w, r)
+	// if !ok {
+	// 	return
+	// }
+	//
+	// type reqType struct {
+	// 	Amount      float64                     `json:"amount"`
+	// 	Direction   db.NullTransactionDirection `json:"direction"`
+	// 	Description pgtype.Text                 `json:"description"`
+	// 	PartyID     pgtype.UUID                 `json:"party_id"`
+	// 	Mode        db.NullTransactionMode      `json:"mode"`
+	// 	CategoryID  pgtype.Int4                 `json:"category_id"`
+	// }
+	// type resType struct{}
+	// var amount pgtype.Numeric
+	// amount.NumericValue()
+	// transaction, err := h.db.CreateTransaction(r.Context(), db.CreateTransactionParams{
+	// 	BusinessID: businessUuid,
+	// 	Amount:     pgtype.Numeric{},
+	// })
 }
