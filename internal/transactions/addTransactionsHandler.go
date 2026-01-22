d2d620e998942d48d4cfd9787d76abe4ee3f4b03
BhatRajendra <rajendrabhat0408@gmai.com>
2026-01-22 10:31:05 +0530

    update receipt no. mandotory only for cash-in

--- internal/transactions/addTransactionsHandler.go
+++ internal/transactions/addTransactionsHandler.go
@@ -77,8 +77,8 @@ func (h *Handler) AddTransactionsHandler(w http.ResponseWriter, r *http.Request)
 	}
 
 	// receipt number
-	if body.ReceiptNo == "" {
-		helpers.RespondWithError(w, http.StatusBadRequest, "bad request: no receipt_no")
+	if body.Direction == db.TransactionDirectionIn && body.ReceiptNo == "" {
+		helpers.RespondWithError(w, http.StatusBadRequest, "bad request: no receipt_no for cash-in")
 		helpers.LogInfo("AddTransactionsHandler", "bad request: no receipt_no")
 		return
 	}
