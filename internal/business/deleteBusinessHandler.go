50412baaf041f6f6cd88bd959a1ed61ace088d44
BhatRajendra <rajendrabhat0408@gmai.com>
2026-03-20 12:04:59 +0530

    update dead state

--- internal/business/deleteBusinessHandler.go
+++ internal/business/deleteBusinessHandler.go
@@ -39,7 +39,8 @@ func (h *Handler) DeleteBusinessHandler(w http.ResponseWriter, r *http.Request) 
 
 	isCancelled := curPlan.SubscriptionsStatus == db.SubscriptionsStatusCanceled ||
 		curPlan.SubscriptionsStatus == db.SubscriptionsStatusTrialEnded ||
-		curPlan.SubscriptionsStatus == db.SubscriptionsStatusInactive
+		curPlan.SubscriptionsStatus == db.SubscriptionsStatusInactive ||
+		curPlan.SubscriptionsStatus == db.SubscriptionsStatusTrialEnded
 
 	if !isCancelled && curPlan.CurrentPlanID.String != configs.FREE_PLAN_ID {
 		helpers.RespondWithError(w, http.StatusForbidden, "can't delete active subscribed business")
